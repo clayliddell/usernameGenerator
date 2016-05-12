5fc0d7db051a2dd4e0b5905d2851864e17a91de9
Clayton Liddell <cbwliddell@gmail.com>
2016-05-11 22:53:59 -0500

    Made methods static to avoid errors with PHP 7; added dictionaries directory

--- usernameGenerate.php
+++ usernameGenerate.php
@@ -7,30 +7,30 @@ class generate {
 	private static $nouns;
 
 	private static function getFiles() {
-		self::$adjectives = file("adjectives.txt");
-		self::$nouns = file("nouns.txt");
+		self::$adjectives = file("dictionaries/adjectives.txt");
+		self::$nouns = file("dictionaries/nouns.txt");
 	}
 
-	public function username() {
+	public static function username() {
 		if(!isset(self::$adjectives) || !isset(self::$nouns)) self::getFiles();
 		$adjectives = self::$adjectives;
 		$nouns = self::$nouns;
 		return trim($adjectives[array_rand($adjectives)]) . trim($nouns[array_rand($nouns)]) . rand(01, 99);
 	}
 
-	public function adjective() {
+	public static function adjective() {
 		if(!isset(self::$adjectives)) self::getFiles();
 		$adjectives = self::$adjectives;
 		return trim($adjectives[array_rand($adjectives)]);
 	}
 
-	public function noun() {
+	public static function noun() {
 		if(!isset(self::$nouns)) self::getFiles();
 		$nouns = self::$nouns;
 		return trim($nouns[array_rand($nouns)]);
 	}
 
-	public function help() {
+	public static function help() {
 		echo "\r\nUsage:\r\n	generate::username() -> Generates random username consisting of a random adjective, noun, and number between 01 and 99.\r\n	generate::adjective() -> Generates a random adjective from a file containing 1000 different adjectives.\r\n	generate::noun() -> Generates a random noun from a file containing 2328 different nouns.";
 	}
 }
